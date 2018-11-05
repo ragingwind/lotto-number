bc4a442314c272c8c2bc3258eba67825cb963fe7
Ahn Dohyung <adhrinae@gmail.com>
2018-11-05 09:29:34 +0900

    Sort generated numbers (#1)
    * Sort generated numbers
    
    * Add test case & test utility for sorted numbers
    
    * Refactor test cases
    
    * Seperate sorting/length tests

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,24 @@
-import test from 'ava'
+import test from 'ava';
 import {generateNumber, normalizeNumber} from '.';
 
-test(async t => {
+function isSorted(current: number, index: number, list: number[]) {
+	const next: number | undefined = list[index + 1];
+	return typeof next === "undefined" || current < next;
+}
+
+test('Generated numbers should be sorted in ascending order', async t => {
+	t.true(generateNumber().every(isSorted));
+	t.true(generateNumber(10).every(isSorted));
+	t.true(generateNumber(10, 45).every(isSorted));
+});
+
+test('Generated numbers should have correct length by the first argument', async t => {
 	t.is(generateNumber().length, 6);
 	t.is(generateNumber(10).length, 10);
 	t.is(generateNumber(10, 45).length, 10);
 });
 
-test(async t => {
+test('normalizeNumber', async t => {
 	t.is(normalizeNumber(NaN, 50, 6), 6);
 	t.is(normalizeNumber('', 50, 7), 7);
 	t.is(normalizeNumber(undefined, 50, 8), 8);
